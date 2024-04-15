cbd40ea49517ecbb772dbcac2793453e05da9aec
appbird <project.appbird@gmail.com>
2024-04-16 06:45:34 +0900

    resolving B2-4

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,6 @@
 use gloo_net::http::Request;
+use gloo_net::websocket;
+use gloo_timers::callback::Interval;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::UnwrapThrowExt;
 use web_sys::HtmlInputElement;
@@ -20,13 +22,34 @@ fn get_value_from_input_event(e: InputEvent) -> String {
     target.value()
 }
 
+
+
 #[function_component]
 fn App() -> Html {
+    let time = use_state(|| 0);
+    let input_value = use_state(|| String::new());
+    
+    let time_ref = time.clone();
+    let input_value_ref = input_value.clone();
+
+    // FIXED: updateされるたびに複数回intervalが作られてしまって困る
+    let interval = Interval::new(100, move || { 
+        let input_value_cloned = input_value.clone();
+        if *time > 0 { time.set(*time - 100); }
+        if *time == 0 {
+            wasm_bindgen_futures::spawn_local(async move {
+                send_api_request(&*input_value_cloned).await.unwrap_throw();
+            });
+        }
+        web_sys::console::log_1(&(*time).to_string().into());
+    });
+    interval.forget();
+
+    // FIXME ここのムーブの問題どうする？
     let oninput = Callback::from(move |input_event: InputEvent| {
         let value = get_value_from_input_event(input_event);
-        wasm_bindgen_futures::spawn_local(async move {
-            send_api_request(&value).await.unwrap_throw();
-        });
+        time_ref.set(600);
+        input_value_ref.set(value.clone());
     });
     html!( 
         <input type={"text"}  {oninput}/>
